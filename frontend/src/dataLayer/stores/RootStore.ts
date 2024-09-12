eb20b826eee05cb9e36ecd2709af1f4694ea508c
ChrisF <56632916+TheBirdEffect@users.noreply.github.com>
2024-09-12 12:02:12 +0200

    Context added

--- frontend/src/dataLayer/stores/RootStore.ts
+++ frontend/src/dataLayer/stores/RootStore.ts
@@ -1,12 +1,13 @@
 import { t, Instance } from 'mobx-state-tree'
-import { Content } from './models/Content'
-import { Section } from './models/Section'
+import { SectionStore } from './SectionStore'
+import { ContentCardStore } from './ContentCardStore'
 
 
 export const RootStore = t.model('RootStore', {
-    content: t.optional(t.array(Content), []),
-    sections: t.optional(t.array(Section), []),
+    sections: t.optional(SectionStore, {}),
+    contentCards: t.optional(ContentCardStore, {})
 })
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface IRootStore extends Instance<typeof RootStore> { }
+
