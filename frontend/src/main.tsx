d642f904a33b665e3c2bcab978caee0b88d3b2b9
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-15 14:47:56 +0200

    added RootStoreProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RootStoreProvider } from './context/RootStoreContext.tsx'
+import { RootStore } from './dataLayer/stores/RootStore.ts'
+
+const useContext = RootStore.create({})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RootStoreProvider value={useContext}>
     <App />
-  </StrictMode>,
+    </RootStoreProvider>
+  </StrictMode>
 )
