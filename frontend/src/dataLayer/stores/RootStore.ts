f0732860c9f7fd03aa52acc52ffd56090190c278
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-15 14:47:42 +0200

    Merged stores

--- frontend/src/dataLayer/stores/RootStore.ts
+++ frontend/src/dataLayer/stores/RootStore.ts
@@ -1,20 +1,11 @@
 import { t, Instance } from 'mobx-state-tree'
 import { SectionStore } from './SectionStore'
-<<<<<<< HEAD
-import { ContentStore } from './ContentStore'
-
-
-export const RootStore = t.model('RootStore', {
-    ContentStore: t.optional(ContentStore, {}),
-    sectionStore: t.optional(SectionStore, {})
-=======
 import { ContentCardStore } from './ContentCardStore'
 
 
 export const RootStore = t.model('RootStore', {
     sections: t.optional(SectionStore, {}),
     contentCards: t.optional(ContentCardStore, {})
->>>>>>> eb20b826eee05cb9e36ecd2709af1f4694ea508c
 })
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
--- frontend/src/dataLayer/stores/SectionStore.ts
+++ frontend/src/dataLayer/stores/SectionStore.ts
@@ -1,20 +1,12 @@
 import { t, Instance } from 'mobx-state-tree';
-<<<<<<< HEAD
 import { Section } from '../models/Section'; // Fix the import path
-import { CardStore } from './CardStore';
-import { ContentCard } from '../models/ContentCard';
-
-export const SectionStore = t.model('SectionStore', {
-    section: t.array(Section),
-    contentCards: t.optional(t.array(ContentCard), []),
-
-=======
-import { Section } from './models/Section'; // Fix the import path
 
 export const SectionStore = t.model('SectionStore', {
     sections: t.array(Section), // Fix the property name
     contentCards: t.array(t.array(t.string)) // Fix the property name
->>>>>>> eb20b826eee05cb9e36ecd2709af1f4694ea508c
 });
 
-export type ISectionStore = Instance<typeof SectionStore>; // Fix the interface name
+// export type ISectionStore = Instance<typeof SectionStore>; // Fix the interface name
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface ISectionStore extends Instance<typeof SectionStore> { } // Fix the interface name
