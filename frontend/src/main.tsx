0e878d2fe404ecfe748fa36630a7ccfee0b91f0c
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-17 23:22:46 +0200

    Added demo data for landing page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,21 @@ import App from './App.tsx'
 import './index.css'
 import { RootStoreProvider } from './context/RootStoreContext.tsx'
 import { RootStore } from './dataLayer/stores/RootStore.ts'
+import { demoLandingData } from './dataLayer/demoData/demoDataSections.ts'
 
-const useContext = RootStore.create({})
+const useContext = RootStore.create(
+  {
+    contentStore: {
+      sections: demoLandingData.sections.map(section => ({
+        ...section,
+        cards: section.cards.map(card => ({
+          ...card,
+          type: "Card"
+        }))
+      }))
+    }
+  }
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
