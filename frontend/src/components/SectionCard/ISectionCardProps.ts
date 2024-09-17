ee1dcbf59c1bd714d8aa9b9bb5a07c16dfea95b8
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-18 00:19:39 +0200

    Added new component isections

---
+++ frontend/src/components/SectionCard/ISectionCardProps.ts
@@ -0,0 +1,3 @@
+export interface ISectionCardProps {
+    id?: string;
+}
---
+++ frontend/src/components/SectionCard/SectionCard.tsx
@@ -0,0 +1,9 @@
+import { ISectionCardProps } from "./ISectionCardProps";
+
+const SectionCard: React.FC<ISectionCardProps> = () => {
+    return (<>
+        This will be the SectionCard component.
+    </>)
+}
+
+export default SectionCard;
