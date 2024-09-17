effb378011afa5888380ab4c7366a0d5f1486ac4
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-18 01:22:53 +0200

    Adjusted StackLayout

--- frontend/src/components/SectionCard/SectionCard.tsx
+++ frontend/src/components/SectionCard/SectionCard.tsx
@@ -2,18 +2,27 @@ import { useCallback } from "react";
 import { StackLayout } from "../../styles/core/ui/StackLayout";
 import { ISectionCardProps } from "./ISectionCardProps";
 
-const SectionCard: React.FC<ISectionCardProps> = ({headline, readMoreLink, children}) => {
-    const handleReadMore = useCallback( () => {
-        console.log(readMoreLink + "Clicked");
-    }, []);
+const SectionCard: React.FC<ISectionCardProps> = ({
+  headline,
+  readMoreLink,
+  children,
+}) => {
+  const handleReadMore = useCallback(() => {
+    console.log(readMoreLink + "Clicked");
+  }, []);
 
-    return (
-        <StackLayout backgroundColor={'rgb(248, 248, 248, 0.7)'}>
-            <h2>{headline}</h2>
-            {children}
-            <button role="button" onClick={handleReadMore}>Read More</button>
-        </StackLayout>
-    )
-}
+  return (
+    <StackLayout backgroundColor={"rgb(248, 248, 248, 0.7)"} margin={'50px 0px'}>
+      <div style={{ top: '-25px', position: 'relative' }}>
+        <h1>{headline}</h1>
+      </div>
+
+      {children}
+      <button role="button" onClick={handleReadMore}>
+        Read More
+      </button>
+    </StackLayout>
+  );
+};
 
 export default SectionCard;
--- frontend/src/styles/core/ui/StackLayout.ts
+++ frontend/src/styles/core/ui/StackLayout.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { PageItemWrapper as Wrapper } from "../../helpers/PageItemWrapper";
-import { theme } from "../../Theme";
 import { device } from "../../BreakPoints";
 
 
@@ -8,11 +7,12 @@ export const StackLayout = styled(Wrapper)`
   id: ${(props) => props.id};
   display: flex;
   gap: 1rem;
-  padding: 10px;
+  margin: ${(props) => props.margin || "0"};
+  padding: ${(props) => props.padding || "10px"};
   align-items: ${(props) => props.alignItems || "center"};
   justify-content: ${(props) => props.justifyContent || "center"};
   flex-direction: ${(props) => props.flexDirection || "column"};
-  background-color: ${(props) => props.backgroundColor || theme.colors.white};
+  background-color: ${(props) => props.backgroundColor};
   border-radius: ${(props) => props.borderRadius || "10px"};
 
   @media (min-width: ${device.xs}) {
