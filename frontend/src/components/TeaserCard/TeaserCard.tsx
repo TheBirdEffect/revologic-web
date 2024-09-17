a2c2dfa27914c6049c5df313bff7cd4753bbb7f8
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-18 01:29:12 +0200

    Image added to teaserCard

--- frontend/src/components/TeaserCard/TeaserCard.tsx
+++ frontend/src/components/TeaserCard/TeaserCard.tsx
@@ -1,13 +1,14 @@
 import { observer } from "mobx-react-lite";
 import { ITeaserCardProps } from "./ITeaserCardProps";
 import { StackLayout } from "../../styles/core/ui/StackLayout";
+import { Image } from "../../styles/core/Image";
 import { theme } from "../../styles/Theme";
 
 const TeaserCard: React.FC<ITeaserCardProps> = ({descriptionImg, headline}) => {
 
     return (
         <StackLayout backgroundColor={theme.colors.teaserPrimary} >
-            <img src={descriptionImg} alt={headline} />
+            <Image src={descriptionImg} alt={headline} />
             <h3>{headline}</h3>
         </StackLayout>
     );
