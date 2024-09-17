c10e59fa5e317cff250c9b440d30bf0a8cbaa0f5
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-17 23:22:26 +0200

    added home component to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from './styles/Global'
 import { theme } from './styles/Theme'
+import { Home } from './pages/Home/Home'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-    
+        <Home />
     </ThemeProvider>
   )
 }
