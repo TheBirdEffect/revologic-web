400b1a0dafcb31ec4ec1beac39e12a4168506883
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-18 01:28:27 +0200

    Styled component image added

---
+++ frontend/src/styles/core/Image.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    src: ${(props) => props.src};
+    alt: ${(props) => props.alt};
+`;
