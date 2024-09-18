43955d1aa13011bc95d9f1a65d43aa0e97af0e86
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-18 18:50:37 +0200

    styled image

--- frontend/src/styles/core/Image.ts
+++ frontend/src/styles/core/Image.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const Image = styled.img`
     width: 100%;
     height: 300px;
+    padding: 10px;
+    border-radius: 10px;
     object-fit: cover;
     src: ${(props) => props.src};
     alt: ${(props) => props.alt};
