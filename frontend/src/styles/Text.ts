4da61cb2c9d274c30bf2046619ca42e8fc499fec
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-21 17:57:47 +0200

    Added styled component for Text

---
+++ frontend/src/styles/Text.ts
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+import { CustomText } from "./helpers/CustomText";
+
+export const Text = styled(CustomText)`
+  size: ${(props) => props.size};
+  color: ${(props) => props.color};
+  font-weight: ${(props) =>
+    props.bold ? "700" : props.weight ? props.weight : "400"};
+`;
