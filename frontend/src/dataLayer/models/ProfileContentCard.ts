e7c88b152d73adf076da8bd9602e4d606d23cfd8
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-23 20:19:08 +0200

    added getter

--- frontend/src/dataLayer/models/ProfileContentCard.ts
+++ frontend/src/dataLayer/models/ProfileContentCard.ts
@@ -10,4 +10,17 @@ export const ProfileContentCard = t.model({
     offers: t.array(t.string),
     interests: t.array(t.string),
     type: t.literal('ProfileContentCard'),
-})
+}).actions(self => ({
+    getProfileContentCard() {
+        return {
+            id: self.id,
+            profileImg: self.profileImg,
+            imgAlt: self.imgAlt,
+            name: self.name,
+            degree: self.degree,
+            position: self.position,
+            offers: self.offers,
+            interests: self.interests
+        }
+    }
+}));
