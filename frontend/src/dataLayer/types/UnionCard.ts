d85154146aeb41767193280602eed36454f507c6
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-23 20:18:58 +0200

    Fixed union cards issue which returned wrong object

--- frontend/src/dataLayer/types/UnionCard.ts
+++ frontend/src/dataLayer/types/UnionCard.ts
@@ -1,13 +1,14 @@
 import { t } from "mobx-state-tree";
 import { TeaserCard } from "../models/TeaserCard";
 import { ContentCard } from "../models/ContentCard";
+import { ProfileContentCard } from "../models/ProfileContentCard";
 
 export const UnionCard = t.union({
     dispatcher: (snapshot) => {
         if (snapshot.type === 'TeaserCard') {
             return TeaserCard;
         } else if (snapshot.type === 'ProfileContentCard') {
-            return ContentCard;
+            return ProfileContentCard;
         } else {
             return ContentCard;
         }
