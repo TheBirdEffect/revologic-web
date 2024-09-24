4ba7eae58453907603b955c7a10393d83d6180c6
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-24 20:04:20 +0200

    converted card types as union and literalt types

--- frontend/src/dataLayer/models/Card/CardBase.ts
+++ frontend/src/dataLayer/models/Card/CardBase.ts
@@ -3,4 +3,6 @@ import { t } from "mobx-state-tree";
 export const CardBase = t.model('CardBase', {
     id: t.identifier,
     target: t.string,
+    // type: t.enumeration(['TeaserCard', 'ProfileCard, ContentCard']),
+    type: t.union(t.literal('TeaserCard'), t.literal('ProfileCard'), t.literal('ContentCard')),
 });
