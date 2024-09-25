37c34f2330ec91aedc47111b828f03f8dacb280a
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-25 19:34:24 +0200

    changed images to reference of ImageModel

--- frontend/src/dataLayer/models/Card/ContentCard.ts
+++ frontend/src/dataLayer/models/Card/ContentCard.ts
@@ -5,7 +5,7 @@ import { ImageModel } from "../ui/Image";
 export const ContentCardModel = CardBase.named('ContentCard').props({
     headline: t.string,
     content: t.string,
-    images: t.maybeNull(t.array(ImageModel)),
+    images: t.maybeNull(t.array(t.reference(ImageModel))),
     type: t.literal('ContentCard')
 }).views(self => ({
     hasGallery: () => self.images !== null && self.images.length > 0
