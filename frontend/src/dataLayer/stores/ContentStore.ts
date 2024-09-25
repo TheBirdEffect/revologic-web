d9e2417b605f78d7370eba3753049e26c2a4f7f6
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-25 18:38:47 +0200

    refactoring

--- frontend/src/dataLayer/stores/ContentStore.ts
+++ frontend/src/dataLayer/stores/ContentStore.ts
@@ -1,11 +1,13 @@
 import { t, Instance } from 'mobx-state-tree';
 import { Section } from '../models/Section'; // Fix the import path
-import { CardBase } from '../models/Card/CardBase';
-import { newDemo } from '../demoData/demoDataSections';
+import { newCards, newDemo } from '../demoData/demoDataSections';
+import { TeaserCardModel } from '../models/Card/TeaserCard';
+import { ProfileContentCardModel } from '../models/Card/ProfileContentCard';
+import { ContentCardModel } from '../models/Card/ContentCard';
 
 export const ContentStore = t.model('SectionStore', {
     sections: t.array(Section),
-    cards: t.array(CardBase)
+    cards: t.optional(t.array(t.union(TeaserCardModel, ProfileContentCardModel, ContentCardModel)), [])
 }).views(self => ({
     getSection(id: string) {
         return self.sections.find(section => section.id === id);
@@ -15,23 +17,7 @@ export const ContentStore = t.model('SectionStore', {
 ContentStore.create({
     sections: newDemo.sections,
     // cards: newDemo.cards
-    cards: [
-        {
-            id: '1',
-            target: 'https://www.google.com',
-            type: 'TeaserCard'
-        },
-        {
-            id: '2',
-            target: 'https://www.google.com',
-            type: 'ProfileContentCard'
-        },
-        {
-            id: '3',
-            target: 'https://www.google.com',
-            type: 'ContentCard'
-        }
-    ]
+    cards: newCards
 });
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -6,42 +6,30 @@ import { ITeaserCardModel } from "../../dataLayer/models/Card/TeaserCard";
 import { StackLayout } from "../../styles/core/ui/StackLayout";
 import { theme } from "../../styles/Theme";
 import { IHomeProps } from "./IHomeProps";
+import { ISectionModel } from "../../dataLayer/models/Section";
+import { ProfileContentCard } from "../../components/ProfileContentCard/ProfileContentCard";
+import { IProfileContentCardModel } from "../../dataLayer/models/Card/ProfileContentCard";
 
 export const Home: React.FC<IHomeProps> = () => {
   const { contentStore } = useRootStore();
-
-  console.log(contentStore.sections);
-  
-
   return (
     <StackLayout backgroundColor={theme.colors.backgroundRich}>
       {contentStore.sections.map((section) => (
-        <SectionCard 
-          key={section.id}
-          data={section}
-          >
-            {section.cards.map((card) => 
-              card.type === "TeaserCard" ? (
-                console.log(section.cards),
-                <TeaserCard data={card as ITeaserCardModel} key={card.id} />
-              ) : null
-            )}
+        <SectionCard key={section.id} data={section as ISectionModel}>
+          {section.cards.map((card) =>
+            card.isTeaserCard() ? (
+              <TeaserCard data={card as ITeaserCardModel} key={card.id} />
+            ) : card.isProfileContentCard() ? (
+              <ProfileContentCard
+                data={card as IProfileContentCardModel}
+                key={card.id}
+              />
+            ) : null
+          )}
         </SectionCard>
       ))}
     </StackLayout>
-    // <StackLayout backgroundColor={theme.colors.backgroundRich}>
-    //   {contentStore.sections.map((section) => (
-    //     <SectionCard 
-    //       key={section.id} 
-    //       headline={section.headline} 
-    //       readMoreLink={section.readMoreLink!}
-    //       >
-    //         <></>
-    //     </SectionCard>
-    //   ))}
-    // </StackLayout>
   );
-
 };
 
 export default observer(Home);
