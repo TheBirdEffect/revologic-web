b9a4b9e1c6a46d43e55fbfe53645ff1bccaaa646
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-25 18:37:53 +0200

    decider methods added

--- frontend/src/dataLayer/models/Card/CardBase.ts
+++ frontend/src/dataLayer/models/Card/CardBase.ts
@@ -5,6 +5,20 @@ export const CardBase = t.model('CardBase', {
     target: t.string,
     // type: t.enumeration(['TeaserCard', 'ProfileCard, ContentCard']),
     type: t.union(t.literal('TeaserCard'), t.literal('ProfileContentCard'), t.literal('ContentCard')),
-});
+}).views(self => ({
+    isTeaserCard() {
+        return self.type === 'TeaserCard';
+    },
+    isProfileContentCard() {
+        return self.type === 'ProfileContentCard';
+    },
+    isContentCard() {
+        return self.type === 'ContentCard';
+    }
+})).actions(self => ({
+    getCard() {
+        return self;
+    }
+}));
 
 export type ICardBaseType = Instance<typeof CardBase>;
