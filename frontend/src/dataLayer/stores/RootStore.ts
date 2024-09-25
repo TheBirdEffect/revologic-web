fff64f696c2301fd65ed14f09994e5461722949e
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-25 19:29:09 +0200

    Added log for snapshot

--- frontend/src/dataLayer/stores/RootStore.ts
+++ frontend/src/dataLayer/stores/RootStore.ts
@@ -1,11 +1,15 @@
-import { t, Instance } from 'mobx-state-tree'
+import { t, Instance, onSnapshot } from 'mobx-state-tree'
 import { ContentStore } from './ContentStore'
 
 export const RootStore = t.model('RootStore', {
     contentStore: t.optional(ContentStore, {})
 })
 
-ContentStore.create()
+const contentStoreInstance = ContentStore.create()
+
+onSnapshot(contentStoreInstance, snapshot => {
+    console.log('Snapshot: ', snapshot)
+})
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface IRootStore extends Instance<typeof RootStore> { }
