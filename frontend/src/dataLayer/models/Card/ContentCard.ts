e3baf5970d31e66ec94a5ad1b9ec8a303e796fdc
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-25 19:28:35 +0200

    Updated Model with mehtod hasGallery

--- frontend/src/dataLayer/models/Card/ContentCard.ts
+++ frontend/src/dataLayer/models/Card/ContentCard.ts
@@ -1,11 +1,14 @@
 import { Instance, t } from "mobx-state-tree";
 import { CardBase } from "./CardBase";
+import { ImageModel } from "../ui/Image";
 
 export const ContentCardModel = CardBase.named('ContentCard').props({
     headline: t.string,
     content: t.string,
-    images: t.maybeNull(t.array(t.string)),
+    images: t.maybeNull(t.array(ImageModel)),
     type: t.literal('ContentCard')
-});
+}).views(self => ({
+    hasGallery: () => self.images !== null && self.images.length > 0
+}));
 
 export type IContentCardModel = Instance<typeof ContentCardModel>;
