e5c73f49668ef1164111e8dafd99962c5dde28fd
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-25 20:05:01 +0200

    Added contentCardModel to union

--- frontend/src/dataLayer/models/Section.ts
+++ frontend/src/dataLayer/models/Section.ts
@@ -1,12 +1,13 @@
 import { Instance, t } from 'mobx-state-tree';
 import { ProfileContentCardModel } from './Card/ProfileContentCard';
 import { TeaserCardModel } from './Card/TeaserCard';
+import { ContentCardModel } from './Card/ContentCard';
 
 export const Section = t.model({
     id: t.identifier,
     headline: t.string,
     readMoreLink: t.maybeNull(t.string),
-    cards: t.array(t.reference(t.union(TeaserCardModel, ProfileContentCardModel))),
+    cards: t.array(t.reference(t.union(TeaserCardModel, ProfileContentCardModel, ContentCardModel))),
 });
 
 export type ISectionModel = Instance<typeof Section>;
