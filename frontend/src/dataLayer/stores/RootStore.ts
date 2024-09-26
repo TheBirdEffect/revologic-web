c894f9b307471d55fe3d1bbe89a42eb80b7f61ed
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-26 18:38:02 +0200

    added ImageStore to root store

--- frontend/src/dataLayer/stores/RootStore.ts
+++ frontend/src/dataLayer/stores/RootStore.ts
@@ -1,13 +1,20 @@
 import { t, Instance, onSnapshot } from 'mobx-state-tree'
 import { ContentStore } from './ContentStore'
+import { ImageStore } from './ImageStore'
 
 export const RootStore = t.model('RootStore', {
-    contentStore: t.optional(ContentStore, {})
+    contentStore: t.optional(ContentStore, {}),
+    ImageStore: t.optional(ImageStore, {})
 })
 
 const contentStoreInstance = ContentStore.create()
+const imageStoreInstance = ImageStore.create()
 
 onSnapshot(contentStoreInstance, snapshot => {
+    console.log('Snapshot: ', snapshot)
+})
+
+onSnapshot(imageStoreInstance, snapshot => {
     console.log('Snapshot: ', snapshot)
 })
 
