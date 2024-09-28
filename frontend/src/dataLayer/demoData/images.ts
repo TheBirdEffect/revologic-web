b46b5ab10c987cc30e0b361c4d06a948352dc320
ChrisF <56632916+TheBirdEffect@users.noreply.github.com>
2024-09-28 13:34:49 +0200

    Added demo data

---
+++ frontend/src/dataLayer/demoData/images.ts
@@ -0,0 +1,29 @@
+export const demoImages = {
+    images: [
+        {
+            id: '1',
+            url: "/img/tech/html.png",
+            alt: "HTML Logo"
+        },
+        {
+            id: '2',
+            url: "/img/tech/react.png",
+            alt: "React Logo"
+        },
+        {
+            id: '3',
+            url: "/img/tech/csharp.png",
+            alt: "JavaScript Logo"
+        },
+        {
+            id: '4',
+            url: "/img/tech/typescript.png",
+            alt: "Typescript Logo"
+        },
+        {
+            id: '5',
+            url: "/img/tech/netcore.png",
+            alt: "Node.js Logo"
+        },
+    ],
+};
