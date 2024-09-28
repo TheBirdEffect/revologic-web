8cf9124411441a55f261b4cc2e1f973e62f3f65b
ChrisF <56632916+TheBirdEffect@users.noreply.github.com>
2024-09-28 16:11:40 +0200

    refactoring

--- frontend/src/components/TeaserCard/ITeaserCardProps.ts
+++ frontend/src/components/TeaserCard/ITeaserCardProps.ts
@@ -1,11 +1,3 @@
-// export interface ITeaserCardProps {
-//     id?: string;
-//     descriptionImg: string;
-//     headline: string;
-//     descriptor?: string;
-//     hasRoundImage?: boolean;
-// }
-
 import { ITeaserCardModel } from "../../dataLayer/models/Card/TeaserCard";
 
 export interface ITeaserCardProps {
