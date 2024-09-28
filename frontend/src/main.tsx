a43f1a32ee7ab917dabac0108badec4f38f57a32
ChrisF <56632916+TheBirdEffect@users.noreply.github.com>
2024-09-28 13:35:07 +0200

    added demo data to useContext

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,18 @@ import './index.css'
 import { RootStoreProvider } from './context/RootStoreContext.tsx'
 import { RootStore } from './dataLayer/stores/RootStore.ts'
 import { newCards, newDemo } from './dataLayer/demoData/demoDataSections.ts'
+import { demoImages } from './dataLayer/demoData/images.ts'
+import { demoImageGalleries } from './dataLayer/demoData/imageGallery.ts'
 
 const useContext = RootStore.create({
   contentStore: {
     sections: newDemo.sections,
     cards: newCards,
   },
+  imageStore: {
+    images: demoImages.images,
+    galleryModel: demoImageGalleries.gallery
+  }
 })
 
 createRoot(document.getElementById('root')!).render(
