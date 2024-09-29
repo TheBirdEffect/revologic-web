227da67dbd5637383db87727c9955840ad4e3ea6
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-30 00:55:31 +0200

    added Footer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { RootStoreProvider } from './context/RootStoreContext.tsx'
-import { RootStore } from './dataLayer/stores/RootStore.ts'
-import { newCards, newDemo } from './dataLayer/demoData/demoDataSections.ts'
-import { demoImages } from './dataLayer/demoData/images.ts'
-import { demoImageGalleries } from './dataLayer/demoData/imageGallery.ts'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RootStoreProvider } from "./context/RootStoreContext.tsx";
+import { RootStore } from "./dataLayer/stores/RootStore.ts";
+import { newCards, newDemo } from "./dataLayer/demoData/demoDataSections.ts";
+import { demoImages } from "./dataLayer/demoData/images.ts";
+import { demoImageGalleries } from "./dataLayer/demoData/imageGallery.ts";
+import { Footer } from "./components/Footer/Footer.tsx";
 
 const useContext = RootStore.create({
   contentStore: {
@@ -15,14 +16,15 @@ const useContext = RootStore.create({
   },
   imageStore: {
     images: demoImages.images,
-    galleryModel: demoImageGalleries.gallery
-  }
-})
+    galleryModel: demoImageGalleries.gallery,
+  },
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RootStoreProvider value={useContext}>
       <App />
+      <Footer />
     </RootStoreProvider>
   </StrictMode>
-)
+);
