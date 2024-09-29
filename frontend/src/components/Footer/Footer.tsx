f9a8ec3be81fdf1d814031ca91fc86de9ba0e6c3
TheBirdEffect <christoph.frischmuth@proton.me>
2024-09-30 00:56:17 +0200

    footer completed

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -2,31 +2,39 @@ import React, { useCallback } from "react";
 import { IFooterProps } from "./IFooterProps";
 import { StackLayout } from "../../styles/core/ui/StackLayout";
 import { TextButton } from "../../styles/core/Button";
+import { theme } from "../../styles/Theme";
 
 export const Footer: React.FC<IFooterProps> = () => {
+  const handlePrivacyClick = useCallback(() => {
+    console.log("opensPrivacyPopup");
+  }, []);
 
-    const handlePrivacyClick = useCallback( () => {
-        console.log("opensPrivacyPopup");
-        
-    }, [])
+  const handleImpressClick = useCallback(() => {
+    console.log("openImpressPopup");
+  }, []);
 
-    const handleImpressClick = useCallback( () => {
-        console.log("openImpressPopup")
-    }, [])
-
-    return (
-        <StackLayout 
-            justifyContent="center" 
-            alignItems="center" 
-            flexDirection="row"
-            backgroundColor="headerFooterBg"
-            >
-            <TextButton onClick={handlePrivacyClick}>
-                Privacy
-            </TextButton>
-            <TextButton onClick={handleImpressClick}>
-                Impress
-            </TextButton>
-        </StackLayout>
-    )
-}
+  return (
+    <>
+    <StackLayout
+      width100
+      height="60px"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="row"
+      backgroundColor={theme.colors.headerFooterBg}
+      hasShadow
+    >
+      <TextButton onClick={handlePrivacyClick}>Privacy</TextButton>
+      <StackLayout
+        noGap
+        noPadding
+        width="2px"
+        borderRadius
+        height="20px"
+        backgroundColor={theme.colors.secondaryTextColor}
+      />
+      <TextButton onClick={handleImpressClick}>Impress</TextButton>
+    </StackLayout>
+    </>
+  );
+};
