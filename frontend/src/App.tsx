b4ec6d1a396ef1b0bada6142dce45a48cd1b8976
ChrisF <56632916+TheBirdEffect@users.noreply.github.com>
2024-10-02 16:22:36 +0200

    Installed simple version of router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,18 @@ import { GlobalStyles } from "./styles/Global";
 import { theme } from "./styles/Theme";
 import { Home } from "./pages/Home/Home";
 import ScrollToTopButton from "./components/Base/ScrollToTop/ScrollToTopButton";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <BrowserRouter basename="/" future={{v7_startTransition: true}}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      {/* <Home /> */}
+      </BrowserRouter>
       <ScrollToTopButton />
     </ThemeProvider>
   );
