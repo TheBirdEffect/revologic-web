7406cc7d8af4d301d0d84ae058b97a2f6da6111f
TheBirdEffect <christoph.frischmuth@proton.me>
2024-10-20 20:56:44 +0200

    Replaced CardResolving logic with CardTypeResolver

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,36 +1,32 @@
 import { observer } from "mobx-react-lite";
 import SectionCard from "../../components/SectionCard/SectionCard";
-import TeaserCard from "../../components/TeaserCard/TeaserCard";
 import { useRootStore } from "../../context/RootStoreContext";
-import { ITeaserCardModel } from "../../dataLayer/models/Card/TeaserCard";
 import { StackLayout } from "../../styles/core/ui/StackLayout";
 import { theme } from "../../styles/Theme";
 import { IHomeProps } from "./IHomeProps";
 import { ISectionModel } from "../../dataLayer/models/Section";
-import { ProfileContentCard } from "../../components/ProfileContentCard/ProfileContentCard";
-import { IProfileContentCardModel } from "../../dataLayer/models/Card/ProfileContentCard";
-import ContentCard from "../../components/ContentCard/ContentCard";
-import { IContentCardModel } from "../../dataLayer/models/Card/ContentCard";
+import { CorpLogo } from "../../styles/core/Image";
+import CardTypeResolver from "../../components/ui/CardResolver/CardTypeResolver";
 
 export const Home: React.FC<IHomeProps> = () => {
   const { contentStore } = useRootStore();
 
+  const { imageStore } = useRootStore();
+  const corpLogo = imageStore.getImageById("CorpLogoMobile");
+  const screen = window.screen.height;
 
   return (
     <StackLayout backgroundColor={theme.colors.backgroundRich}>
+      <StackLayout
+        paddingTop={`${screen * 0.13}px`}
+        paddingBottom={`${screen * 0.13}px`}
+      >
+        <CorpLogo src={corpLogo?.url} alt={corpLogo?.alt} />
+      </StackLayout>
       {contentStore.sections.map((section) => (
         <SectionCard key={section.id} data={section as ISectionModel}>
           {section.cards.map((card) =>
-            card.isTeaserCard() ? (
-              <TeaserCard data={card as ITeaserCardModel} key={card.id} />
-            ) : card.isProfileContentCard() ? (
-              <ProfileContentCard
-                data={card as IProfileContentCardModel}
-                key={card.id}
-              />
-            ) : card.isContentCard() ? (
-              <ContentCard data={card as IContentCardModel} key={card.id} />
-            ) : null
+            <CardTypeResolver data={card} key={card.id} />
           )}
         </SectionCard>
       ))}
