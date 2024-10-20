921891188b098afef6c49ce83ba0bcc2857d6ced
TheBirdEffect <christoph.frischmuth@proton.me>
2024-10-20 20:57:08 +0200

    implemented service component

--- frontend/src/pages/Services/Services.tsx
+++ frontend/src/pages/Services/Services.tsx
@@ -1,12 +1,22 @@
+import SectionCard from "../../components/SectionCard/SectionCard";
+import CardResolver from "../../components/ui/CardResolver/CardTypeResolver";
+import { useRootStore } from "../../context/RootStoreContext";
+import { ISectionModel } from "../../dataLayer/models/Section";
 import { StackLayout } from "../../styles/core/ui/StackLayout";
 import { IServicesProps } from "./IServicesProps";
 
 export const Services: React.FC<IServicesProps> = () => {
-    return(
-        <StackLayout>
-            <h1>
-                Services
-            </h1>
-        </StackLayout>
-    )
-}
+  const { contentStore } = useRootStore();
+  const services = contentStore.getSection("services");
+
+  return (
+    <StackLayout noGap>
+      <h1>Services</h1>
+      <SectionCard noOverline noReadMoreButton data={services as ISectionModel}>
+        {services?.cards.map((card) => (
+            <CardResolver data={card} key={card.id} />
+        ))} 
+      </SectionCard>
+    </StackLayout>
+  );
+};
